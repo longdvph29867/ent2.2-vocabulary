3a67e67420b6ac07d89afa9fc100d7f757eaa688
longdv ph29867 <emchuaroinhe@gmail.com>
2023-05-17 11:56:57 +0700

    up bt

---
+++ src/Components/Search/ent1.js
@@ -0,0 +1,332 @@
+
+export const arrEnt = [
+    'speed',
+    'tailgate',
+    'talk on the phone',
+    'text while driving',
+    'weave through traffic',
+    'not stop at red lights',
+    'not signal when turning ',
+    'pass in a no-passing zone',
+
+    'Honk their horns',
+    'Stare at other drivers',
+    'Gesture at other drivers',
+    'Flash their lights at other drivers',
+    'Pay attention',
+    'Observe the speed limit',
+    'Maintain a safe following distance',
+
+    'A full-size sedan',
+    'A compact car',
+    'A convertible',
+    'A sports car',
+    'A station wagon',
+    'A minivan/van',
+    'An SUV',
+    'A luxury car',
+
+    'Headlight',
+    'Engine',
+    'Hood',
+    'Windshield wiper',
+    'Windshield',
+    'Turn signal light',
+    'Bumper',
+    'Tire',
+
+    'Sunroof',
+    'Trunk',
+    'Taillight',
+    'Side view mirror',
+    'Window',
+    'Door',
+
+
+    'Steering wheel',
+    'Horn',
+    'Dashboard',
+    'Gas pedal',
+    'Brake pedal',
+    'Clutch',
+    'Gearshift',
+    'Rearview mirror',
+    'Emergency brake',
+    'Seatbelt',
+
+    'Turn on',
+    'Turn off',
+    'Pick up',
+    'Fill up',
+    'Drop off',
+];
+export const arrEnt2 = [
+    {
+        eng: 'speed',
+        vie: 'đi quá tốc độ'
+    },
+    {
+        eng: 'tailgate',
+        vie: 'bám đuôi'
+    },
+    {
+        eng: 'talk on the phone',
+        vie: 'sử dụng điện thoại'
+    },
+    {
+        eng: 'text while driving',
+        vie: 'nhắn tin khi lái xe'
+    },
+    {
+        eng: 'weave through traffic',
+        vie: 'lạng lách, đánh võng'
+    },
+    {
+        eng: 'not stop at red lights',
+        vie: 'không dừng đèn đỏ'
+    },
+    {
+        eng: 'not signal when turning',
+        vie: 'không xi nhan khi rẽ'
+    },
+    {
+        eng: 'pass in a no-passing zone',
+        vie: 'vượt ở đường cấm vượt'
+    },
+    {
+        eng: 'Honk their horns',
+        vie: 'bóp còi'
+    },
+    {
+        eng: 'Stare at other drivers',
+        vie: 'nhìn chằm chằm vào lái xe khác'
+    },
+    {
+        eng: 'Gesture at other drivers',
+        vie: 'ra hiệu với lái xe khác'
+    },
+    {
+        eng: 'Flash their lights at other drivers',
+        vie: 'nháy đèn vào lái xe khác'
+    },
+    {
+        eng: 'Pay attention',
+        vie: 'tập trung, chú ý'
+    },
+    {
+        eng: 'Observe the speed limit',
+        vie: 'quan sát tốc độ cho phép'
+    },
+    {
+        eng: 'Maintain a safe following distance',
+        vie: 'duy trì khoảng cách an toàn'
+    },
+    {
+        eng: 'A full-size sedan',
+        vie: 'Xe 4 chỗ, có cốp'
+    },
+    {
+        eng: 'A compact car',
+        vie: 'Xe 4 chỗ, không cốp'
+    },
+    {
+        eng: 'A convertible',
+        vie: 'Xe mui trần'
+    },
+    {
+        eng: 'A sports car',
+        vie: 'Xe thể thao'
+    },
+    {
+        eng: 'A station wagon',
+        vie: 'Xe 7 chỗ'
+    },
+    {
+        eng: 'A minivan/van',
+        vie: 'Xe bán tải/xe tải'
+    },
+    {
+        eng: 'An SUV',
+        vie: 'Xe địa hình'
+    },
+    {
+        eng: 'A luxury car',
+        vie: 'Xe hạng sang'
+    },
+    {
+        eng: 'Headlight',
+        vie: 'đèn pha'
+    },
+    {
+        eng: 'Engine',
+        vie: 'động cơ'
+    },
+    {
+        eng: 'Hood',
+        vie: 'nắp cabô'
+    },
+    {
+        eng: 'Windshield wiper',
+        vie: 'cần gạt nước'
+    },
+    {
+        eng: 'Windshield',
+        vie: 'kính chắn gió'
+    },
+    {
+        eng: 'Turn signal light',
+        vie: 'đèn xi nhan'
+    },
+    {
+        eng: 'Bumper',
+        vie: 'bộ phận giảm xóc'
+    },
+    {
+        eng: 'Tire',
+        vie: 'lốp xe'
+    },
+    {
+        eng: 'Sunroof',
+        vie: 'cửa sổ trời'
+    },
+    {
+        eng: 'Trunk',
+        vie: 'cốp xe'
+    },
+    {
+        eng: 'Taillight',
+        vie: 'đèn hậu'
+    },
+    {
+        eng: 'Side view mirror',
+        vie: 'gương chiếu hậu'
+    },
+    {
+        eng: 'Window',
+        vie: 'cửa sổ'
+    },
+    {
+        eng: 'Door',
+        vie: 'cửa chính'
+    },
+    {
+        eng: 'Steering wheel',
+        vie: 'vô lăng'
+    },
+    {
+        eng: 'Horn',
+        vie: 'còi'
+    },
+    {
+        eng: 'Dashboard',
+        vie: 'bảng điều khiển'
+    },
+    {
+        eng: 'Gas pedal',
+        vie: 'chân gas'
+    },
+    {
+        eng: 'Brake pedal',
+        vie: 'chân phanh'
+    },
+    {
+        eng: 'Clutch',
+        vie: 'chân côn'
+    },
+    {
+        eng: 'Gearshift',
+        vie: 'cần gạt số'
+    },
+    {
+        eng: 'Rearview mirror',
+        vie: 'gương chiếu hậu'
+    },
+    {
+        eng: 'Emergency brake',
+        vie: 'phanh khẩn cấp'
+    },
+    {
+        eng: 'Seatbelt',
+        vie: 'dây an toàn'
+    },
+    {
+        eng: 'Turn on',
+        vie: 'bật'
+    },
+    {
+        eng: 'Turn off',
+        vie: 'tắt'
+    },
+    {
+        eng: 'Pick up',
+        vie: 'đến lấy'
+    },
+    {
+        eng: 'Fill up',
+        vie: 'đổ đầy'
+    },
+    {
+        eng: 'Drop off',
+        vie: 'nhận, trả'
+    },
+]
+
+// speed: đi quá tốc độ
+// tailgate: bám đuôi
+// talk on the phone: sử dụng điện thoại
+// text while driving: nhắn tin khi lái xe
+// weave through traffic: lạng lách, đánh võng
+// not stop at red lights: không dừng đèn đỏ
+// not signal when turning : không xi nhan khi rẽ
+// pass in a no-passing zone: vượt ở đường cấm vượt
+
+// Honk their horns: bóp còi
+// Stare at other drivers: nhìn chằm chằm vào lái xe khác
+// Gesture at other drivers: ra hiệu với lái xe khác
+// Flash their lights at other drivers: nháy đèn vào lái xe khác
+// Pay attention: tập trung, chú ý
+// Observe the speed limit: quan sát tốc độ cho phép
+// Maintain a safe following distance: duy trì khoảng cách an toàn
+
+// A full-size sedan: Xe 4 chỗ, có cốp
+// A compact car: Xe 4 chỗ, không cốp
+// A convertible: Xe mui trần
+// A sports car: Xe thể thao
+// A station wagon: Xe 7 chỗ
+// A minivan/van: Xe bán tải/xe tải
+// An SUV: Xe địa hình
+// A luxury car: Xe hạng sang
+
+// Headlight: đèn pha
+// Engine: động cơ
+// Hood: nắp cabô
+// Windshield wiper: cần gạt nước
+// Windshield: kính chắn gió
+// Turn signal light: đèn xin nhan
+// Bumper: bộ phận giảm xóc
+// Tire:lốp xe
+
+// Sunroof: cửa sổ trời
+// Trunk: cốp xe
+// Taillight: đèn hậu
+// Side view mirror: gương chiếu hậu
+// Window: cửa sổ
+// Door: cửa chính
+
+
+// Steering wheel: vô lăng
+// Horn: còi
+// Dashboard: bảng điều khiển
+// Gas pedal: chân gas
+// Brake pedal: chân phanh
+// Clutch: chân côn
+// Gearshift: cần gạt số
+// Rearview mirror: gương chiếu hậu
+// Emergency brake: phanh khẩn cấp
+// Seatbelt: dây an toàn
+
+// Turn on: bật
+// Turn off: tắt
+// Pick up: đến lấy
+// Fill up: đổ đầy
+// Drop off: nhận, trả
